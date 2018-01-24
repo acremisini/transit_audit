a746d6d024719165ab4ace5abf45fb5e8a5c0165
acremisini <18541774+acremisini@users.noreply.github.com>
2018-01-24 07:54:31 -0500

    adding test ""

--- train_audit_new.py
+++ train_audit_new.py
@@ -1,67 +1,46 @@
 import pandas as pd
 from datetime import datetime
-import sqlite3
 import numpy as np
-import const
-import copy
-#import matplotlib.pyplot as plt
 import json
+import pymysql.cursors
 import sys
-import mysql.connector
-from mysql.connector import errorcode
+sys.path.insert(0, '/home/transit/www/data/secure')
+import database
+from collections import OrderedDict
 
 #usage <timespan_start=YYYY-MM-DD%HH:MM:SS&timespan_end=YYYY-MM-DD%HH:MM:SS&stats=Boolean&station_list=comma,separated,stations>
 #note: this returns trips that both begin and end within the timespan, sorted for each station by time_announced
 
 def connect_and_return_data(timespan_start, timespan_end):
-    # try:
-    #     print('connecting...')
-    #     db = mysql.connector.connect(user=const.user, password=const.password,
-    #                                   host=const.host,
-    #                                   database=const.database)
-    #     query = """
-    #         SELECT * FROM trainTrip
-    #         WHERE dateadded >= '{}' AND dateadded <= '{}'
-    #         ORDER BY StationID, Time1_Train, dateadded;
-    #         """.format(timespan_start, timespan_end)
-    #     df = pd.read_sql_query(query, db)
-    #
-    #     print('done')
-    # except mysql.connector.Error as err:
-    #     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    #         print("Something is wrong with your user name or password")
-    #     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    #         print("Database does not exist")
-    #     else:
-    #         print(err)
-    # else:
-    #     cnx.close()
-    #ordering like this to avoid edge case where train 1 is announced as arriving in x mins, then train 2
-    #is announced as arriving in y < x mins, and neither are ghost trains
-    query = """
-    SELECT * FROM trainTrip
-    WHERE dateadded >= '{}' AND dateadded <= '{}'
-    ORDER BY StationID, Time1_Train, dateadded;
-    """.format(timespan_start, timespan_end)
-    try:
-        db = sqlite3.connect('trips.db', isolation_level=None)
-        df = pd.read_sql_query(query, db)
-        db.close()
-    except:
-        print('error connecting to db')
 
-    return df
+    connection = pymysql.connect(host=database.host,
+                                 user=database.user,
+                                 password=database.password,
+                                 db=database.database,
+                                 unix_socket = database.unix_socket,
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+            query = """
+                SELECT * FROM trainTrip
+                WHERE dateadded >= '{}' AND dateadded <= '{}'
+                ORDER BY StationID, Time1_Train, dateadded;
+                """.format(timespan_start, timespan_end)
+            result = pd.read_sql_query(query, connection)
+    finally:
+        connection.close()
+        return result
 
 # filter out ghosts
 def clean_train_trip(df):
-
     df_filter = pd.DataFrame()
     i = 0
     filter_list = list()
     l = len(df) - 1
     # df_filter = df.where(df['Time1_Est'] == 0 and df[''])
     while i < l:
-        if df.at[i, 'Time1_Est'] != 0 and df.at[i + 1,'Time1_Est'] == 0 and df.at[i,'Time1_Train'] == df.at[i + 1,'Time1_Train']:
+        if int(df.at[i, 'Time1_Est']) != 0 and int(df.at[i + 1,'Time1_Est']) == 0 and int(df.at[i,'Time1_Train']) == int(df.at[i + 1,'Time1_Train']):
             filter_list.append(df.loc[i])
             filter_list.append(df.loc[i+1])
             i += 2
@@ -81,22 +60,22 @@ def make_data_dicts(df, stats, station_list):
         try:
             if df[i]['StationID'] in station_list:
                 if df[i]['StationID'] not in station_dict:
-                    station_dict[df[i]['StationID']] = pd.DataFrame(columns=['trip_num',
+                    station_dict[str(df[i]['StationID'])] = pd.DataFrame(columns=['trip_num',
                                                                                  'time_announced',
                                                                                  'projected_duration',
                                                                                  'time_arrived',
                                                                                  'actual_duration',
                                                                                  'time_since_last_train'])
                 else:
-                    announced = datetime.strptime(df[i]['dateadded'], '%Y-%m-%d %H:%M:%S')
-                    arrived = datetime.strptime(df[i + 1]['dateadded'], '%Y-%m-%d %H:%M:%S')
+                    announced = datetime.strptime(str(df[i]['dateadded']), '%Y-%m-%d %H:%M:%S')
+                    arrived = datetime.strptime(str(df[i + 1]['dateadded']), '%Y-%m-%d %H:%M:%S')
                     headway = (arrived - announced).total_seconds()
-                    report = pd.DataFrame({'trip_num': df[i]['trip_num'],
-                                           'time_announced': df[i]['dateadded'],
-                                           'projected_duration': df[i]['Time1_Est'],
-                                           'time_arrived': df[i+1]['dateadded'],
-                                           'actual_duration': headway,
-                                           'time_since_last_train': df[i+1]['time_since_last']}, index=[0])
+                    report = pd.DataFrame({'trip_num': int(df[i]['trip_num']),
+                                           'time_announced': str(df[i]['dateadded']),
+                                           'projected_duration': int(df[i]['Time1_Est']),
+                                           'time_arrived': str(df[i+1]['dateadded']),
+                                           'actual_duration': int(headway),
+                                           'time_since_last_train': int(df[i+1]['time_since_last'])}, index=[0])
                     station_dict[df[i]['StationID']] = station_dict[df[i]['StationID']].append(report)
                 if stats:
                     if 'general' not in stats_dict:
@@ -104,12 +83,12 @@ def make_data_dicts(df, stats, station_list):
                     if df[i]['StationID'] not in stats_dict:
                         stats_dict[df[i]['StationID']] = {'headways' : [], 'headway_deltas' : []}
                     else:
-                        if str(df[i+1]['time_since_last']) != 'nan':
-                            stats_dict[df[i]['StationID']]['headways'].append(df[i+1]['time_since_last'])
-                            stats_dict['general']['headways'].append((df[i+1]['time_since_last']))
-                        if str(df[i + 1]['est_error']) != 'nan':
-                            stats_dict[df[i]['StationID']]['headway_deltas'].append(df[i + 1]['est_error'])
-                            stats_dict['general']['headway_deltas'].append((df[i + 1]['est_error']))
+                        if str(df[i+1]['time_since_last']) != 'None':
+                            stats_dict[df[i]['StationID']]['headways'].append(int(df[i+1]['time_since_last']))
+                            stats_dict['general']['headways'].append((int(df[i+1]['time_since_last'])))
+                        if str(df[i + 1]['est_error']) != 'None':
+                            stats_dict[df[i]['StationID']]['headway_deltas'].append(int(df[i + 1]['est_error']))
+                            stats_dict['general']['headway_deltas'].append((int(df[i + 1]['est_error'])))
                 i += 2
             else:
                 i += 1
@@ -127,7 +106,7 @@ def make_data_dicts(df, stats, station_list):
 def make_json_string(stats_dict, station_dict, stats, timespan_start, timespan_end):
     if stats:
         json_dict = \
-            {"transit_report": {
+            {"\"transit_report\"": {
                     "timespan_start" : timespan_start,
                     "timespan_end" : timespan_end,
                     "data" : {
@@ -154,52 +133,52 @@ def make_json_string(stats_dict, station_dict, stats, timespan_start, timespan_e
                     for s in list(station_dict):
                         if k2 == 'summary_statistics':
                             if 'GENERAL' not in json_dict[k][k1][k2]:
-                                json_dict[k][k1][k2]['GENERAL'] = {'headways' : {'mean': np.mean(stats_dict['general']['headways']),
-                                                                    'std_dev' : np.std(stats_dict['general']['headways']),
-                                                                    'min' : np.amin(stats_dict['general']['headways']),
-                                                                    'max' : np.amax(stats_dict['general']['headways']),
-                                                                    'median' : np.percentile(stats_dict['general']['headways'], 50),
-                                                                    '25th_ptile' : np.percentile(stats_dict['general']['headways'], 25),
-                                                                    '75th_ptile' : np.percentile(stats_dict['general']['headways'], 75)},
-                                                                   'headway_deltas' : {'mean': np.mean(stats_dict['general']['headway_deltas']),
-                                                                    'std_dev' : np.std(stats_dict['general']['headway_deltas']),
-                                                                    'min' : np.amin(stats_dict['general']['headway_deltas']),
-                                                                    'max' : np.amax(stats_dict['general']['headway_deltas']),
-                                                                    'median' : np.percentile(stats_dict['general']['headway_deltas'], 50),
-                                                                    '25th_ptile' : np.percentile(stats_dict['general']['headway_deltas'], 25),
-                                                                    '75th_ptile' : np.percentile(stats_dict['general']['headway_deltas'], 75)}}
-                            json_dict[k][k1][k2][s] = {'headways': {'mean': np.mean(stats_dict[s]['headways']),
-                                                                    'std_dev' : np.std(stats_dict[s]['headways']),
-                                                                    'min' : np.amin(stats_dict[s]['headways']),
-                                                                    'max' : np.amax(stats_dict[s]['headways']),
-                                                                    'median' : np.percentile(stats_dict[s]['headways'], 50),
-                                                                    '25th_ptile' : np.percentile(stats_dict[s]['headways'], 25),
-                                                                    '75th_ptile' : np.percentile(stats_dict[s]['headways'], 75)
+                                json_dict[k][k1][k2]['GENERAL'] = {'headways' : {'mean': str(np.mean(stats_dict['general']['headways'])),
+                                                                    'std_dev' : str(np.std(stats_dict['general']['headways'])),
+                                                                    'min' : str(np.amin(stats_dict['general']['headways'])),
+                                                                    'max' : str(np.amax(stats_dict['general']['headways'])),
+                                                                    'median' : str(np.percentile(stats_dict['general']['headways'], 50)),
+                                                                    '25th_ptile' : str(np.percentile(stats_dict['general']['headways'], 25)),
+                                                                    '75th_ptile' : str(np.percentile(stats_dict['general']['headways'], 75))},
+                                                                   'headway_deltas' : {'mean': str(np.mean(stats_dict['general']['headway_deltas'])),
+                                                                    'std_dev' : str(np.std(stats_dict['general']['headway_deltas'])),
+                                                                    'min' : str(np.amin(stats_dict['general']['headway_deltas'])),
+                                                                    'max' : str(np.amax(stats_dict['general']['headway_deltas'])),
+                                                                    'median' : str(np.percentile(stats_dict['general']['headway_deltas'], 50)),
+                                                                    '25th_ptile' : str(np.percentile(stats_dict['general']['headway_deltas'], 25)),
+                                                                    '75th_ptile' : str(np.percentile(stats_dict['general']['headway_deltas'], 75))}}
+                            json_dict[k][k1][k2][s] = {'headways': {'mean': str(np.mean(stats_dict[s]['headways'])),
+                                                                    'std_dev' : str(np.std(stats_dict[s]['headways'])),
+                                                                    'min' : str(np.amin(stats_dict[s]['headways'])),
+                                                                    'max' : str(np.amax(stats_dict[s]['headways'])),
+                                                                    'median' : str(np.percentile(stats_dict[s]['headways'], 50)),
+                                                                    '25th_ptile' : str(np.percentile(stats_dict[s]['headways'], 25)),
+                                                                    '75th_ptile' : str(np.percentile(stats_dict[s]['headways'], 75))
                                                                     },
                                                        'headway_deltas': {
-                                                                        'mean': np.mean(stats_dict[s]['headway_deltas']),
-                                                                        'std_dev' : np.std(stats_dict[s]['headway_deltas']),
-                                                                        'min' : np.amin(stats_dict[s]['headway_deltas']),
-                                                                        'max' : np.amax(stats_dict[s]['headway_deltas']),
-                                                                        'median' : np.percentile(stats_dict[s]['headway_deltas'], 50),
-                                                                        '25th_ptile' : np.percentile(stats_dict[s]['headway_deltas'], 25),
-                                                                        '75th_ptile' : np.percentile(stats_dict[s]['headway_deltas'], 75)
+                                                                        'mean': str(np.mean(stats_dict[s]['headway_deltas'])),
+                                                                        'std_dev' : str(np.std(stats_dict[s]['headway_deltas'])),
+                                                                        'min' : str(np.amin(stats_dict[s]['headway_deltas'])),
+                                                                        'max' : str(np.amax(stats_dict[s]['headway_deltas'])),
+                                                                        'median' : str(np.percentile(stats_dict[s]['headway_deltas'], 50)),
+                                                                        '25th_ptile' : str(np.percentile(stats_dict[s]['headway_deltas'], 25)),
+                                                                        '75th_ptile' : str(np.percentile(stats_dict[s]['headway_deltas'], 75))
                                                                     }
                                                        }
                         elif k2 == 'trips_by_station':
                             json_dict[k][k1][k2][s] = station_dict[s].to_dict(orient='records')
 
-    json_str = json.dumps(json_dict, indent=3, separators=(',', ': '))
+    json_str = json.dumps(json_dict, indent=3, separators=(',', ': '), sort_keys=True)
     print(json_str)
 
-# timespan_start = sys.argv[1]
-# timespan_end = sys.argv[2]
-# stats = sys.argv[3]
-# station_list = sys.argv[4].split(",")
-timespan_start = "2017-12-22 12:00:00"
-timespan_end = "2017-12-22 23:00:00"
-stats = True
-station_list = ['GVT-NB', 'GVT-SB', "BLK-NB", "BLK-SB","CVC-NB", "CVC-SB","MLK-NB", "MLK-SB"]
+timespan_start = sys.argv[1]
+timespan_end = sys.argv[2]
+stats = bool(sys.argv[3])
+station_list = sys.argv[4].split("%")
+# timespan_start = "2017-12-22 12:00:00"
+# timespan_end = "2017-12-22 23:00:00"
+# stats = True
+# station_list = ['GVT-NB', 'GVT-SB', "BLK-NB", "BLK-SB","CVC-NB", "CVC-SB","MLK-NB", "MLK-SB"]
 
 #grab data
 df = connect_and_return_data(timespan_start, timespan_end)
